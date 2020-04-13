9f5432c5aaa49a2617cf40e7ca0df4642bd4c148
mdomingu <mdomingu@gmailcom>
2020-04-12 22:45:41 -0300

    agrego getters y setters

--- src/frsf/died/git/agenda/Agenda.java
+++ src/frsf/died/git/agenda/Agenda.java
@@ -6,5 +6,23 @@ public class Agenda {
 	
 	private String detalle;
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+	
+	
+
 
 }
